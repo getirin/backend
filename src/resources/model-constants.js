f051fc4f5d49dd34bc806d0263fe2336a720836b
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-02-17 15:35:43 +0300

    add finished status to order and carrier requests.

--- src/resources/model-constants.js
+++ src/resources/model-constants.js
@@ -7,11 +7,13 @@ module.exports = {
     WAITING: 0,
     OBTAINED: 1,
     CANCELED_BY_USER: 2,
+    FINISHED: 3,
   },
   carrierRequestStatuses: {
     WAITING: 0,
     OBTAINED: 1,
     CANCELED_BY_USER: 2,
     CANCELED_BY_CARRIER: 3,
+    FINISHED: 4,
   },
 };
