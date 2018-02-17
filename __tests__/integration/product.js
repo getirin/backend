b811c90c00e31f551ca79eff70a012f411ea9ab2
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-02-17 14:32:21 +0300

    add aggregation function for calculating product total prices by given id. - integrations tests with jest and the new test_mongodb url

---
+++ __tests__/integration/product.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { setupTestLogger } = require('../helper');
+const log = setupTestLogger();
+const { test: { database: { connectionString } }} = require('../../src/config');
+const Product = require('../../src/models/Product');
+
+describe('product integration tests', () => {
+  describe('ProductModel', () => {
+    beforeAll(async () => {
+      await mongoose.connect(connectionString);
+    });
+
+    const testData = [
+      { name: 'Domates', price: 3 },
+      { name: 'Biber', price: 7 },
+      { name: 'Patlıcan', price: 2 }
+    ];
+
+    it('should save without any problems', async () => {
+      for(let i = 0; i < testData.length; i++) {
+        expect(await new Product(testData[i]).save()).toHaveProperty('_id');
+      }
+    });
+
+    it('should properly calculate totalPrice for recorded products', async () => {
+      const saved = await Promise.all(testData.map((data) => new Product(data).save()));
+      const totalPrice = testData.reduce((acc, p) => acc + p.price, 0);
+
+      expect(await Product.calculateTotalPriceOfProducts(saved.map(p => p._id))).toEqual(totalPrice);
+    });
+
+    afterEach(async () => {
+      await Product.remove();
+    });
+
+    afterAll(async () => {
+      await mongoose.disconnect();
+    });
+  })
+});
--- src/models/Product.js
+++ src/models/Product.js
@@ -5,4 +5,20 @@ const ProductJoiSchema = require('../schemas/models/Product');
 const productSchema = new mongoose.Schema(Joigoose.convert(ProductJoiSchema), { timestamps: true });
 productSchema.index({ name: 1 }, { unique: true });
 
+/**
+ * Given a list of ids, calculates the total price for the matched products.
+ * @param ids
+ * @return {Promise<number>}
+ */
+productSchema.statics.calculateTotalPriceOfProducts = async function(ids){
+  return this.aggregate([
+    { $match: { _id: { $in: ids.map(id => mongoose.Types.ObjectId(id)) } } },
+    { $group: { _id: null, totalPrice: { $sum: '$price' } } }
+  ]).exec()
+    .then(aggregate => {
+      if(aggregate && aggregate.length > 0) return Promise.resolve(aggregate[0].totalPrice);
+      return Promise.rejeect(new Error("Couldn't calculate the total price."));
+    });
+};
+
 module.exports = mongoose.model('Product', productSchema);
