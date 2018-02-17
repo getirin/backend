e7954e5128de6cffa85acdd4979198585f210cb9
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-02-17 07:30:40 +0300

    add deepSet and pick functions to use when picking apart joi schemas in other places.

--- __tests__/unit/utils.js
+++ __tests__/unit/utils.js
@@ -1,4 +1,4 @@
-const { mergeRoutes, deepGet } = require('../../src/utils');
+const { mergeRoutes, deepGet, deepSet, pick } = require('../../src/utils');
 
 describe('utils', () => {
   describe('mergeRoutes', () => {
@@ -31,6 +31,53 @@ describe('utils', () => {
       expect(deepGet(testObj, 'x')).toBe(undefined);
       expect(deepGet(testObj, 'x.y')).toBe(undefined);
       expect(deepGet(testObj, 'f.g.h.j.k.l.m.n.o')).toBe(undefined);
+    });
+  });
+
+  describe('deepSet', () => {
+    const testObj = { a: 5, b: { c: 10 } };
+
+    it('should not mutate objects', () => {
+      expect(deepSet(testObj, 'a', 10)).not.toEqual(testObj);
+      expect(deepSet(testObj, 'b.c', 5)).not.toEqual(testObj);
+    });
+
+    it('should set values correctly', () => {
+      expect(deepSet(testObj, 'a', 10)).toEqual({ a: 10, b: { c: 10 } });
+      expect(deepSet(testObj, 'b.c', 5)).toEqual({ a: 5, b: { c: 5 } });
+    });
+
+    it('should set non defined keys correctly', () => {
+      expect(deepSet(testObj, 'd', 73)).toEqual({ a: 5, b: { c: 10 }, d : 73});
+      expect(deepSet(testObj, 'd.e', 73)).toEqual({ a: 5, b: { c: 10 }, d : { e: 73 }});
+    });
+  });
+
+  describe('pick', () => {
+    const testObj = {
+      a: 'hello',
+      b: { c: { d: 5 } },
+      d: { e: 4 },
+      f: { g: { h: 'world' } },
+    };
+
+    it('should return empty object for non defined keys', () => {
+      expect(pick(testObj, ['z'])).toEqual({});
+    });
+
+    it('should return an object with selected keys only for valid keys', () => {
+      expect(pick(testObj, ['a', 'b'])).toEqual({ a: testObj.a, b: testObj.b });
+      expect(pick(testObj, ['a', 'c'])).toEqual({ a: testObj.a, c: testObj.c });
+      expect(pick(testObj, ['d'])).toEqual({ d: testObj.d });
+      expect(pick(testObj, 'd')).toEqual({ d: testObj.d });
+    });
+
+    it('should not fail with undefined obj', () => {
+      expect(pick(undefined, [])).toEqual({});
+    });
+
+    it('should not fail with invalid keys', () => {
+      expect(pick(testObj, undefined)).toEqual({});
     });
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -27,6 +27,37 @@ function deepGet(obj, path){
 }
 
 /**
+ * Sets the given value at the key given. Doesn't mutate the target obj.
+ * @param obj the object to set the property of
+ * @param path a dot delimited string or an array of keys
+ * @param value the value to set at the object's target path.
+ * @return {*}
+ */
+function deepSet(obj, path, value){
+  if(!path || path.length === 0) return obj;
+  if(!obj || obj.constructor !== Object) return obj;
+  if(path.constructor !== Array && path.constructor !== String) return obj;
+  const keys = path.constructor === Array ? [...path] : path.split('.');
+
+  return keys.reduceRight((v, key, idx, keys) => {
+    return { ...deepGet(obj, keys.slice(0, idx)), ...{ [key]: v } };
+  }, value);
+}
+
+/**
+ * Picks the given keys from the object, creates a new one.
+ * @param obj the object to pick the keys from.
+ * @param keys the keys to pick from the object.
+ */
+function pick(obj, keys){
+  if(!keys || keys.length === 0 || ![Array, String].includes(keys.constructor)) return {};
+  if(!obj || obj.constructor !== Object) return {};
+  const k = keys.constructor === Array ? keys : [keys];
+
+  return k.reduce((acc, key) => deepSet(acc, key, deepGet(obj, key)), {});
+}
+
+/**
  * Merges the given route to the original routes array by comparing path and method.
  * @param original the original routes array
  * @param toMerge the routes to override the original array with.
@@ -64,6 +95,8 @@ module.exports = {
   requireFolderWithKeys,
   createMongooseEnumValidator,
   deepGet,
+  deepSet,
   mergeRoutes,
-  createRouteGenerator
+  createRouteGenerator,
+  pick
 };
