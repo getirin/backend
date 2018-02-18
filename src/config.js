67f98ed83119cc4adb7ae5ce1c266f71ace81fb1
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-02-18 04:58:35 +0300

    add method for matching orders with a dynamically configurable sorting algorithm.

--- src/config.js
+++ src/config.js
@@ -32,6 +32,36 @@ module.exports = {
     orderMatch: {
       // The radius that determines which markets can supply a specific order
       closeMarketMaxDistance: 1000,
+      // Sorting algorithm settings for listing the available orders.
+      sorting: {
+        // desc order.
+        order: -1,
+        // different weights to possible parameters.
+        factors: {
+          time: 10,
+          price: 10,
+          markets: 25,
+          distance: 55,
+        },
+        // create weight groups according to your needs.
+        groups: {
+          /**
+           * For time we have 3 groups, if the order is lesser than 5 mins, we give it a %60 weight in time parameter.
+           * If its greater than 10 mins, we give it %30, if its between 5-10 mins then it will be %10
+           */
+          time: {
+            default: 10,
+            factors: [{ matchers: { $lt: 5 * 60 * 1000 }, factor: 60 }, { matchers: { $gt: 10 * 60 * 1000 }, factor: 30 }]
+          },
+          /**
+           * Products which are more expensive than 15 TL will have %70 weight. Otherwise its %30
+           */
+          price: {
+            default: 30,
+            factors: [{ matchers: { $gt: 15 }, factor: 70 }]
+          }
+        }
+      }
     }
   }
 };
--- src/models/OrderMarketMatch.js
+++ src/models/OrderMarketMatch.js
@@ -14,13 +14,15 @@
  */
 const mongoose = require('mongoose');
 const Market = require('./Market');
-const { application: { orderMatch: { closeMarketMaxDistance: maxDistance } } } = require('../config');
+const { orderStatuses } = require('../resources/model-constants');
+const { application: { orderMatch: { closeMarketMaxDistance: maxDistance, sorting } } } = require('../config');
 
 const orderMarketMatchSchema = new mongoose.Schema({
   order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   closeMarkets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Market' }],
   destination: { type: [Number], index: '2dsphere' },
+  totalPrice: { type: Number }
 }, { timestamps: true });
 
 // Can be read from the config file etc.
@@ -44,8 +46,132 @@ orderMarketMatchSchema.statics.createMarketMatchForOrder = async function(order,
     order: order._id,
     user: order.user,
     destination: order.destination,
-    closeMarkets: markets
+    closeMarkets: markets,
+    totalPrice: order.totalPrice
   });
+};
+
+function createScoreCalculationQueryGenerator(factors, groups){
+  //
+  function multiplyWithFactor(obj, factor){
+    return { $multiply: [ obj, factor ] };
+  }
+
+  function divideConstantQueryGenerator(field, divideValue){
+    return { $divide: [ `$${field}`, divideValue ]  };
+  }
+
+  function branchQueryGenerator(field, group){
+    // If there are no factors, we can just return a constant.
+    if(!group.factors || group.factors.length === 0) return group.default || 0;
+    // Calculate the divider for the whole grup.
+    const calculateGroupDivideConstant =
+      // sum of the all factors + the default
+      group.factors.map(f => f.factor).reduce((acc, v) => acc + v, group.default || 0);
+
+    /**
+     * Given a matcher in our config, converts it to mongo query
+     * @return {*}
+     */
+    function convertMatcher(field, key, value){
+      if(!key || !['$gt', '$gte', '$lt', '$lte'].includes(key)) return null;
+      return { [`${key}`]: [ `$${field}`, value ] };
+    }
+
+    return {
+      $switch: {
+        // For each factors in the array, convert them to a switch case branch.
+        branches: group.factors.map(({ matchers, factor }) => ({
+          // do the conversion by looking at the keys of the matchers value of each group.
+          case: { $and: Object.keys(matchers).map((key) => convertMatcher(field, key, matchers[key])).filter(x => x) },
+          then: factor / calculateGroupDivideConstant
+        })),
+        default: (group.default || 0) / calculateGroupDivideConstant
+      }
+    };
+  }
+
+  function callAlgorithmFunction(name, cfg){
+    if(!factors[name]) return null;
+    const { type, field } = cfg;
+    const factor = factors[name];
+
+    if(type === 'divideConstant') return multiplyWithFactor(divideConstantQueryGenerator(field, cfg.constant), factor);
+    else if(type === 'branch') return multiplyWithFactor(branchQueryGenerator(field, groups[name]), factor);
+    return null;
+  }
+
+  return function weightCalculationQueryGenerator(configuration){
+    return {
+      $add:
+        Object.keys(configuration)
+          .map((key) => callAlgorithmFunction(key, configuration[key]))
+          .filter(x => x)
+    };
+  };
+}
+const scoreCalculationQueryGenerator = createScoreCalculationQueryGenerator(sorting.factors, sorting.groups);
+
+/**
+ * Given a radius finds orders that can be satisfied without leaving this radius.
+ * e.g. you can go the to Market(supplier), fulfill the order, and deliver it to the order destination without
+ * leaving your set radius.
+ * The returned records are sorted by a configurable algorithm where you can associate different weights to 4 variables
+ *  - distance of the person from your location represented in %
+ *  - count of intersection between markets in your range and order's range in %
+ *  - price of the order in TL
+ *  - amount of seconds passed since order creation
+ * @param location the center point of the radius.
+ * @param maxDistance the radius of the search in meters.
+ * @param order the sorting order as int, default is read from config.
+ * @return {Promise<Aggregate>}
+ */
+orderMarketMatchSchema.statics.findMatchesWithRadius = async function(location, maxDistance, order = sorting.order){
+  const locationCloseMarkets = Market.nearbyOfLocation(location, maxDistance);
+  // Define how to handle sorting fields, if they are convertible to percentages
+  // just divide them with the given constant, otherwise we need to use branching to group the values into some
+  // factors. For example distance and closeMarketCount are convertible to percentages so we just do so.
+  // but with time and prices we need custom defined groups.
+  const configuration = {
+    distance: { field: 'distance', type: 'divideConstant', constant: maxDistance },
+    markets: { field: 'marketIntersection', type: 'divideConstant', constant: locationCloseMarkets.length },
+    price: { field: 'totalPrice', type: 'branch' },
+    time: { field: 'timePassedMs', type: 'branch' }
+  };
+
+  const results = await this.aggregate([
+    // Limit to the order destinations within the given radius
+    { $geoNear: { near: location, spherical: true, distanceField: 'distance', maxDistance } },
+    {
+      $project: {
+        // Create a field that holds how many of the user's close markets collide with orders
+        marketIntersection: { $size: { $setIntersection: ['$closeMarkets', locationCloseMarkets] } },
+        // Create a field that holds how many milliseconds it passed since the order was created.
+        timePassedMs: { $subtract: [ new Date(), '$createdAt' ] },
+        order: 1,
+      },
+    },
+    { $match: { marketIntersection: { $gt: 0 } } },
+    // The the orders from the foreign table.
+    { $lookup: { from: 'orders', localField: 'order', foreignField: '_id', as: 'order' } },
+    // Match the status of the order.
+    { $match: { 'order.0.status': orderStatuses.WAITING } },
+    // Calculate the score for the match.
+    {
+      $project: {
+        order: 1,
+        marketIntersection: 1,
+        distance: 1,
+        totalPrice: 1,
+        timePassedMs: 1,
+        // Dynamically generating a mongo query for calculating the score
+        score: scoreCalculationQueryGenerator(configuration)
+      },
+    },
+    { $sort: { score: order } }
+  ]);
+
+  return results.map((item) => item.order[0]);
 };
 
 module.exports = mongoose.model('OrderMarketMatch', orderMarketMatchSchema);
